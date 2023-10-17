8b5680acbe9e2550a762812944aec7564aa194b7
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-17 12:27:39 +0200

    add contact route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
           path:"",component: HomePageComponent
+  },
+  {
+          path:"contact",component: ContactComponent
   },
   {
           path: "home",component: HomeComponent
